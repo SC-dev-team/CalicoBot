e01fe47fb17f44a70e408bc8dd03e7e76796ff96
FanaticPond3462 <111404437+FanaticPond3462@users.noreply.github.com>
2023-08-27 17:55:21 +0900

    all linted

--- embeds.py
+++ embeds.py
@@ -8,40 +8,40 @@ def gen_help(name):
     """Genereate help embed
 
     Returns:
-        HelpEmbed: insert your response
-    """    
-    HelpEmbed = discord.Embed(
+        help_embed: insert your response
+    """
+    help_embed = discord.Embed(
         title="CalicoBot Help", color=discord.Color.green(), timestamp=datetime.now())
-    HelpEmbed.add_field(name="", value="prefix `mod!`", inline=False)
-    HelpEmbed.add_field(name="", value="`help` 今表示されてるやつを表示します", inline=False)
-    HelpEmbed.add_field(name="", value="`ban` メンバーをBANします", inline=False)
-    HelpEmbed.add_field(name="", value="`gban` メンバーをグローバルBANします", inline=False)
-    HelpEmbed.add_field(name="", value="`kick` メンバーをキックします", inline=False)
-    HelpEmbed.add_field(name="", value="`unban` メンバーをunBANします", inline=False)
-    HelpEmbed.add_field(name="", value="prefix `/`", inline=False)
-    HelpEmbed.add_field(
+    help_embed.add_field(name="", value="prefix `mod!`", inline=False)
+    help_embed.add_field(name="", value="`help` 今表示されてるやつを表示します", inline=False)
+    help_embed.add_field(name="", value="`ban` メンバーをBANします", inline=False)
+    help_embed.add_field(name="", value="`gban` メンバーをグローバルBANします", inline=False)
+    help_embed.add_field(name="", value="`kick` メンバーをキックします", inline=False)
+    help_embed.add_field(name="", value="`unban` メンバーをunBANします", inline=False)
+    help_embed.add_field(name="", value="prefix `/`", inline=False)
+    help_embed.add_field(
         name="", value="`member ban` メンバーをBANします", inline=False)
-    HelpEmbed.add_field(
+    help_embed.add_field(
         name="", value="`member gban` メンバーをグローバルBANします", inline=False)
-    HelpEmbed.add_field(
+    help_embed.add_field(
         name="", value="`member kick` メンバーをキックします", inline=False)
-    HelpEmbed.add_field(
+    help_embed.add_field(
         name="", value="`member unban` メンバーをunBANします", inline=False)
-    HelpEmbed.set_footer(name)
+    help_embed.set_footer(name)
 
-    return HelpEmbed
+    return help_embed
 
 
 def gen_managelog(target, executer, method, reason, name):
     """Generate log embed
 
     Returns:
-        LogEmbed: insert your response
+        log_embed: insert your response
     """
-    LogEmbed = discord.Embed(
+    log_embed = discord.Embed(
         title=f"{target}を{method}しました", color=discord.Color.red(), timestamp=datetime.now())
-    LogEmbed.add_field(name="担当者", value=executer)
-    LogEmbed.add_field(name="理由", value=f"`{reason}`")
-    LogEmbed.set_footer(text=name)
+    log_embed.add_field(name="担当者", value=executer)
+    log_embed.add_field(name="理由", value=f"`{reason}`")
+    log_embed.set_footer(text=name)
 
-    return LogEmbed
+    return log_embed
--- main.py
+++ main.py
@@ -19,18 +19,18 @@ def load_user_karma(file_path="./user_karma_list.json"):
     """load user karma form path to json file
 
     Args:
-        file_path (str, optional): Please set to path of json file. Defaults to "./user_karma_list.json".
+        file_path (str, optional): Please set to path of json file. 
+        Defaults to "./user_karma_list.json".
 
     Returns:
         dict: result ob json.load
-    """    
+    """
     if os.path.isfile(file_path):
-        with open(file_path, "r") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             # load = fileobj to dict #loads = string to dict
-            user_karma_list = json.load(file)
-        return user_karma_list
-    else:
-        return dict()
+            _user_karma_list = json.load(file)
+        return _user_karma_list
+    return {}
 
 
 def save_user_karma(content, file_path="./user_karma_list.json"):
@@ -38,9 +38,10 @@ def save_user_karma(content, file_path="./user_karma_list.json"):
 
     Args:
         content (dict): users' karma list
-        file_path (str, optional): Please set to path of json file. Defaults to "./user_karma_list.json".
-    """    
-    with open(file_path, "w+") as file:
+        file_path (str, optional): Please set to path of json file. 
+        Defaults to "./user_karma_list.json".
+    """
+    with open(file_path, "w+", encoding="utf-8") as file:
         # dump = dict to fileobj #loads = dict to string
         json.dump(content, file)
 
@@ -49,17 +50,33 @@ def save_user_karma(content, file_path="./user_karma_list.json"):
 user_karma_list = load_user_karma()
 
 # on ready
+
+
 @bot.event
 async def on_ready():
     """Call when discord bot's status is ready
-    """    
+    """
     print("起動しました")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server"))
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server")
+    )
     try:
         synced = await bot.tree.sync()
         print(f"{len(synced)}個のコマンドを同期しました")
-    except Exception as e:
-        print(e)
+    except discord.Forbidden:
+        print(
+            "Don't have permission in some server. Couldn't sync command in some server...")
+    except discord.HTTPException:
+        print("HTTPException. Please restart later...")
+    except discord.TranslationError:
+        print("TranslationError. Please restart later...")
+    except discord.MissingApplicationID:
+        print("Missing ApplicationID. Exiting...")
+        raise
+    except discord.CommandSyncFailure:
+        print("Some commands worng . Exiting...")
+        raise
 
 # help command
 
@@ -67,77 +84,101 @@ async def on_ready():
 @bot.command(name="help")
 async def help_command(ctx):
     """When user use mod! help,Invoke this
-    """        
-    helpEmbed = embeds.gen_help(ctx.guild.name)  # set footer
-    await ctx.send(embed=helpEmbed)
+    """
+    help_embed = embeds.gen_help(ctx.guild.name)  # set footer
+    await ctx.send(embed=help_embed)
 
 
 @bot.tree.command(name="bothelp", description="このbotのヘルプを表示します")
 async def help_tree_command(interaction: discord.Interaction):
     """When user use /bothelp,Invoke this
-    """    
-    helpEmbed = embeds.gen_help(interaction.guild.name)  # set footer
-    await interaction.response.send_message(embed=helpEmbed)
+    """
+    help_embed = embeds.gen_help(interaction.guild.name)  # set footer
+    await interaction.response.send_message(embed=help_embed)
 
 # @bot.command
 
 
 @bot.command(name="ban")
 @commands.has_permissions(ban_members=True)
-async def ban_command(ctx, member: discord.Member, reason: str = "無し", deleteMessageDays: int = 0):
+async def ban_command(ctx, member: discord.Member,
+                      reason: str = "無し", delete_message_days: int = 0):
     """When user use mod! ban,Invoke this
-    """    
-    await member.ban(reason=reason, delete_message_days=deleteMessageDays)
-    await ctx.send(embed=embeds.gen_managelog(member.display_name, ctx.author.mention, "BAN", reason, ctx.guild.name))
+    """
+    await member.ban(reason=reason, delete_message_days=delete_message_days)
+    await ctx.send(embed=embeds.gen_managelog(
+        member.display_name,
+        ctx.author.mention,
+        "BAN",
+        reason,
+        ctx.guild.name
+    ))
 
 
 @bot.command(name="gban")
 @commands.has_permissions(ban_members=True)
-async def ban_command(ctx, member: discord.Member, reason: str = "無し", deleteMessageDays: int = 0):
+async def gban_command(ctx, member: discord.Member,
+                       reason: str = "無し", delete_message_days: int = 0):
     """When user use mod! gban,Invoke this
-    """   
-
+    """
     if not member.id in user_karma_list:
         user_karma_list[member.id] = 0
     user_karma_list[member.id] -= 1
 
-    await member.ban(reason=reason, delete_message_days=deleteMessageDays)
-    await ctx.send(embed=embeds.gen_managelog(member.display_name, ctx.author.mention, "グローバルBAN", reason, ctx.guild.name))
+    await member.ban(reason=reason, delete_message_days=delete_message_days)
+    await ctx.send(embed=embeds.gen_managelog(member.display_name,
+                                              ctx.author.mention,
+                                              "グローバルBAN", reason, ctx.guild.name))
 
 
 @bot.command(name="kick")
 @commands.has_permissions(kick_members=True)
 async def kick_command(ctx, member: discord.Member, reason: str = "無し"):
     """When user use mod! kick,Invoke this
-    """ 
+    """
     await member.kick(reason=reason)
-    await ctx.send(embed=embeds.gen_managelog(member.display_name, ctx.author.mention, "kick", reason, ctx.guild.name))
+    await ctx.send(embed=embeds.gen_managelog(
+        member.display_name,
+        ctx.author.mention,
+        "kick",
+        reason,
+        ctx.guild.name)
+    )
 
 
 @bot.command(name="unban")
 @commands.has_permissions(ban_members=True)
 async def unban_command(ctx, member: discord.User, reason: str = "無し"):
     """When user use mod! unban,Invoke this
-    """ 
+    """
     guild = discord.Guild
 
     await guild.unban(self=ctx.guild, user=member, reason=reason)
-    await ctx.send(embed=embeds.gen_managelog(member.display_name, ctx.author.mention, "unBAN", reason, ctx.guild.name))
-
+    await ctx.send(embed=embeds.gen_managelog(member.display_name,
+                                              ctx.author.mention,
+                                              "unBAN",
+                                              reason,
+                                              ctx.guild.name))
 
 # @bot.tree.command
+
+
 class ManageMember(app_commands.Group):
     """ManegeMember Class
 
         Its function will be registered when bot will start
-    """    
+    """
+
     def __init__(self, name: str):
         super().__init__(name=name)
 
     @app_commands.command(description="指定したメンバーをBANします")
     @app_commands.checks.has_permissions(ban_members=True)
     @app_commands.describe(days="BANしたユーザーのメッセージ削除の期間")
-    async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str = "無し", days: int = 0):
+    async def ban(self,
+                  interaction: discord.Interaction,
+                  member: discord.Member,
+                  reason: str = "無し", days: int = 0):
         """When user use /member ban,Invoke this
 
         Args:
@@ -145,14 +186,21 @@ class ManageMember(app_commands.Group):
             member (discord.Member): member object in discord
             reason (str, optional): string of ban reason. Defaults to "無し".
             days (int, optional): int of days in delete messages. Defaults to 0.
-        """        
+        """
         await member.ban(reason=reason, delete_message_days=days)
-        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name, interaction.user.mention, "BAN", reason, interaction.guild.name))
+        await interaction.response.send_message(embed=embeds.gen_managelog(
+            member.display_name,
+            interaction.user.mention,
+            "BAN",
+            reason,
+            interaction.guild.name
+            ))
 
     @app_commands.command(description="指定したメンバーをグローバルBANします")
     @app_commands.checks.has_permissions(ban_members=True)
     @app_commands.describe(days="BANしたユーザーのメッセージ削除の期間")
-    async def gban(self, interaction: discord.Interaction, member: discord.Member, reason: str = "無し", days: int = 0):
+    async def gban(self, interaction: discord.Interaction, member: discord.Member,
+                   reason: str = "無し", days: int = 0):
         """When user use /member gban,Invoke this
 
         Args:
@@ -160,56 +208,76 @@ class ManageMember(app_commands.Group):
             member (discord.Member): member object in discord
             reason (str, optional): string of global ban reason. Defaults to "無し".
             days (int, optional): int of days in delete messages. Defaults to 0.
-        """        
+        """
         if not member.id in user_karma_list:
             user_karma_list[member.id] = 0
         user_karma_list[member.id] -= 1
 
         await member.ban(reason=reason, delete_message_days=days)
-        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name, interaction.user.mention, "グローバルBAN", reason, interaction.guild.name))
+        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name,
+                                                                           interaction.user.mention,
+                                                                           "グローバルBAN",
+                                                                           reason,
+                                                                           interaction.guild.name
+                                                                           ))
 
     @app_commands.command(description="指定したメンバーをKICKします")
     @app_commands.checks.has_permissions(kick_members=True)
-    async def kick(self, interaction: discord.Interaction, member: discord.Member, reason: str = "無し"):
+    async def kick(self, interaction: discord.Interaction, member: discord.Member
+                   ,reason: str = "無し"):
         """When user use /member kick,Invoke this
 
         Args:
             interaction (discord.Interaction): None desctiption
             member (discord.Member): member object in discord
             reason (str, optional): string of kick reason. Defaults to "無し".
-        """        
+        """
         await member.kick(reason=reason)
-        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name, interaction.user.mention, "kick", reason, interaction.guild.name))
+        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name,
+                                                                           interaction.user.mention,
+                                                                           "kick",
+                                                                           reason,
+                                                                           interaction.guild.name
+                                                                           ))
 
     @app_commands.command(description="指定したメンバーをunBANします")
     @app_commands.checks.has_permissions(ban_members=True)
-    async def unban(self, interaction: discord.Interaction, member: discord.User, reason: str = "無し"):
+    async def unban(self, interaction: discord.Interaction, member: discord.User,
+                    reason: str = "無し"):
         """When user use /member unban,Invoke this
 
         Args:
             interaction (discord.Interaction): None desctiption
             member (discord.Member): member object in discord
             reason (str, optional): string of unban reason. Defaults to "無し".
-        """        
+        """
         guild = interaction.guild
 
         await guild.unban(user=member, reason=reason)
-        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name, interaction.user.mention, "unBAN", reason, interaction.guild.name))
-
+        await interaction.response.send_message(embed=embeds.gen_managelog(member.display_name,
+                                                                           interaction.user.mention,
+                                                                           "unBAN",
+                                                                           reason,
+                                                                           interaction.guild.name
+                                                                           ))
 
 # @bot.event
+
+
 @bot.event
 async def on_guild_join():
     """Generate presence when status changed
-    """    
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server"))
+    """
+    await bot.change_presence(status=discord.Status.online,
+                              activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server"))
 
 
 @bot.event
 async def on_guild_remove():
     """Generate presence when status changed
-    """    
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server"))
+    """
+    await bot.change_presence(status=discord.Status.online,
+                              activity=discord.Game(name=f"mod!help | {len(bot.guilds)} server"))
 
 bot.tree.add_command(ManageMember("member"))
 try:
